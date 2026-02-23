5060f1e4069d4e22df9ce425f0a913517784c38e
0126prashant <prashantsom75@gmail.com>
2026-02-23 10:43:47 +0530

    Change the format for 21st.dev

---
+++ src/demos/default.tsx
@@ -0,0 +1,9 @@
+import FluidSpatialHero from "@/components/ui/component";
+
+export default function Demo() {
+    return (
+        <div className="w-full h-full relative">
+            <FluidSpatialHero />
+        </div>
+    );
+}
